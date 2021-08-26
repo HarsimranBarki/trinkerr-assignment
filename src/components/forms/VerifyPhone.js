e8ae288e933ffa308bb4a52427f0e1387a8189dd
HarsimranBarki <harsimransinghbarki@gmail.com>
2021-08-26 17:52:57 +0530

    added input validation

--- src/components/forms/VerifyPhone.js
+++ src/components/forms/VerifyPhone.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 const VerifyPhone = () => {
+  const [phone, setPhone] = useState("");
+  // Regex For Formatting Input Should Be Number
+  const onlyNumbers = (e) => {
+    e.preventDefault();
+    let value = e.target.value.replace(/[^\d]/g, "");
+    setPhone(value);
+  };
+
+  const validateInput = (e, size, message) => {
+    e.preventDefault();
+    if (phone.length !== size) return;
+  };
+
   return (
     <div>
-      <form className="mt-10 ">
+      <form className="mt-10 " onSubmit={(e) => validateInput(e)}>
         <input
           type="text"
-          className="form-input px-4 py-2 border-gray-300 rounded mt-2"
+          className="form-input px-4 py-2 border-gray-300 rounded mt-2 shadow-inner"
           placeholder="Enter Your Number"
+          value={phone}
+          onChange={(e) => onlyNumbers(e, "phone")}
+          autoFocus
         />
         <br />
-        <button className="bg-indigo-600 text-indigo-50 px-5 py-2 font-medium rounded mt-5 hover:bg-indigo-700 transition">
+        <button
+          type="submit"
+          className="bg-indigo-600 text-indigo-50 px-5 py-2  font-medium rounded mt-5 hover:bg-indigo-700 transition"
+        >
           Get OTP
         </button>
       </form>
