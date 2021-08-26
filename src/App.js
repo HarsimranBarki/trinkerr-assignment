1add25bd253e44380d845a8efc46a3412f4128a9
HarsimranBarki <harsimransinghbarki@gmail.com>
2021-08-26 16:17:49 +0530

    added userContext

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-function App() {
-  return <div className=""></div>;
-}
+import { useMemo, useState } from "react";
+import { UserContext } from "./libs/userContext";
+
+const App = () => {
+  const [user, setUser] = useState({});
+  const providerValue = useMemo(() => ({ user, setUser }), [user, setUser]);
+
+  return (
+    <UserContext.Provider value={providerValue}>
+      <div className=""></div>
+    </UserContext.Provider>
+  );
+};
 
 export default App;
