4834988b86d87ea47b7b1d0c9b713e612a82c20f
HarsimranBarki <harsimransinghbarki@gmail.com>
2021-09-02 16:34:45 +0530

    update VerifyPhone test with new conventions and cleaned imports

--- src/tests/VerifyPhone.test.js
+++ src/tests/VerifyPhone.test.js
@@ -1,66 +1,57 @@
-import React from "react";
-import { mount } from "enzyme";
-import renderer from "react-test-renderer";
-import VerifyPhone from "../components/forms/VerifyPhone";
-
-import Enzyme from "enzyme";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import Enzyme, { shallow } from "enzyme";
+import toJson from "enzyme-to-json";
+import React from "react";
+import VerifyPhone from "../components/forms/VerifyPhone";
 import { UserProvider } from "../libs/userContext";
-import { render, screen, fireEvent } from "@testing-library/react";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe("VerifyPhone", () => {});
-it("VerifyPhone: should render correctly", () => {
-  const tree = renderer
-    .create(
-      mount(
-        <UserProvider>
-          <VerifyPhone />
-        </UserProvider>
-      )
-    )
-    .toJSON();
+describe("VerifyPhone", () => {
+  it("VerifyPhone: should render correctly", () => {
+    const wrapper = shallow(
+      <UserProvider>
+        <VerifyPhone />
+      </UserProvider>
+    );
 
-  expect(tree).toMatchSnapshot();
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it("VerifyPhone: should match the input values of phone", () => {
+    render(
+      <UserProvider>
+        <VerifyPhone />
+      </UserProvider>
+    );
+
+    const inputPhone = screen.queryByTestId("input");
+
+    fireEvent.change(inputPhone, { target: { value: "1234567890" } });
+    expect(inputPhone.value).toBe("1234567890");
+
+    let vals = inputPhone.value.length;
+
+    expect(vals).not.toBeNull();
+    expect(vals).not.toBeGreaterThan(10);
+    expect(vals).not.toBeLessThan(10);
+  });
+
+  it("VerifyPhone: should not allow null values", () => {
+    render(
+      <UserProvider>
+        <VerifyPhone />
+      </UserProvider>
+    );
+
+    const buttonPhone = screen.queryByTestId("button");
+    const inputPhone = screen.queryByTestId("input");
+
+    inputPhone.value = "";
+    fireEvent.click(buttonPhone);
+    let vals = inputPhone.value.length;
+    // buttonPhone.simulate("click");
+    expect(vals).not.toBeNull();
+  });
 });
-
-// describe("test phone input", () => {
-//   it("Updates on change", () => {
-//     render(
-//       <UserProvider>
-//         <VerifyPhone />
-//       </UserProvider>
-//     );
-
-//     const buttonPhone = screen.queryByTestId("button");
-//     const inputPhone = screen.queryByTestId("input");
-
-//     fireEvent.change(inputPhone, { target: { value: "1234567890" } });
-//     // buttonPhone.simulate("click");
-//     expect(inputPhone.value).toBe("1234567890");
-//     let vals = inputPhone.value.length;
-//     expect(vals).not.toBeNull();
-//     expect(vals).not.toBeGreaterThan(10);
-//     expect(vals).not.toBeLessThan(10);
-//   });
-// });
-
-// describe("test phone input empty value", () => {
-//   it("Updates on change", () => {
-//     render(
-//       <UserProvider>
-//         <VerifyPhone />
-//       </UserProvider>
-//     );
-
-//     const buttonPhone = screen.queryByTestId("button");
-//     const inputPhone = screen.queryByTestId("input");
-
-//     inputPhone.value = "";
-//     fireEvent.click(buttonPhone);
-//     let vals = inputPhone.value.length;
-//     // buttonPhone.simulate("click");
-//     expect(vals).not.toBeNull();
-//   });
-// });
