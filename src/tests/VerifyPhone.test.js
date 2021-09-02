8d175b91543cdebdd73338cd50e91a00544113d7
HarsimranBarki <harsimransinghbarki@gmail.com>
2021-09-02 16:29:34 +0530

    added enzyme-to-json for snapshot testing of enzyme components

--- src/tests/VerifyPhone.test.js
+++ src/tests/VerifyPhone.test.js
@@ -1,5 +1,6 @@
-import React, { useContext } from "react";
+import React from "react";
 import { mount } from "enzyme";
+import renderer from "react-test-renderer";
 import VerifyPhone from "../components/forms/VerifyPhone";
 
 import Enzyme from "enzyme";
@@ -9,56 +10,57 @@ import { render, screen, fireEvent } from "@testing-library/react";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-it("test phone", () => {
-  const TestComponent = () => {
-    return <VerifyPhone />;
-  };
-  const wrapper = mount(
-    <UserProvider>
-      <TestComponent />
-    </UserProvider>
-  );
+describe("VerifyPhone", () => {});
+it("VerifyPhone: should render correctly", () => {
+  const tree = renderer
+    .create(
+      mount(
+        <UserProvider>
+          <VerifyPhone />
+        </UserProvider>
+      )
+    )
+    .toJSON();
 
-  expect(wrapper.find("button")).toBeTruthy();
-  expect(wrapper.find("input")).toBeTruthy();
+  expect(tree).toMatchSnapshot();
 });
 
-describe("test phone input", () => {
-  it("Updates on change", () => {
-    render(
-      <UserProvider>
-        <VerifyPhone />
-      </UserProvider>
-    );
+// describe("test phone input", () => {
+//   it("Updates on change", () => {
+//     render(
+//       <UserProvider>
+//         <VerifyPhone />
+//       </UserProvider>
+//     );
 
-    const buttonPhone = screen.queryByTestId("button");
-    const inputPhone = screen.queryByTestId("input");
+//     const buttonPhone = screen.queryByTestId("button");
+//     const inputPhone = screen.queryByTestId("input");
 
-    fireEvent.change(inputPhone, { target: { value: "1234567890" } });
-    // buttonPhone.simulate("click");
-    expect(inputPhone.value).toBe("1234567890");
-    let vals = inputPhone.value.length;
-    expect(vals).not.toBeNull();
-    expect(vals).not.toBeGreaterThan(10);
-    expect(vals).not.toBeLessThan(10);
-  });
-});
+//     fireEvent.change(inputPhone, { target: { value: "1234567890" } });
+//     // buttonPhone.simulate("click");
+//     expect(inputPhone.value).toBe("1234567890");
+//     let vals = inputPhone.value.length;
+//     expect(vals).not.toBeNull();
+//     expect(vals).not.toBeGreaterThan(10);
+//     expect(vals).not.toBeLessThan(10);
+//   });
+// });
 
-describe("test phone input empty value", () => {
-  it("Updates on change", () => {
-    render(
-      <UserProvider>
-        <VerifyPhone />
-      </UserProvider>
-    );
+// describe("test phone input empty value", () => {
+//   it("Updates on change", () => {
+//     render(
+//       <UserProvider>
+//         <VerifyPhone />
+//       </UserProvider>
+//     );
 
-    const buttonPhone = screen.queryByTestId("button");
-    const inputPhone = screen.queryByTestId("input");
+//     const buttonPhone = screen.queryByTestId("button");
+//     const inputPhone = screen.queryByTestId("input");
 
-    inputPhone.value = "";
-    fireEvent.click(buttonPhone);
-    let vals = inputPhone.value.length;
-    // buttonPhone.simulate("click");
-    expect(vals).not.toBeNull();
-  });
-});
+//     inputPhone.value = "";
+//     fireEvent.click(buttonPhone);
+//     let vals = inputPhone.value.length;
+//     // buttonPhone.simulate("click");
+//     expect(vals).not.toBeNull();
+//   });
+// });
